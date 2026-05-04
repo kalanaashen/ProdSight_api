589d820f0bdaae992583b9a1496cb40c1c7e72d6
kalanaashen <northernwolf1472@gmail.com>
2026-05-04 10:32:56 +0530

    add user schema

---
+++ PRODSIGHT_API/src/models/users.js
@@ -0,0 +1,39 @@
+const { min } = require('lodash');
+const mongoose=require('mongoose');
+const Joi=require('joi');
+const User=mongoose.model('User',new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minlength:10,
+        maxlength:50,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:255,
+        trim:true
+    }
+
+}));
+
+
+async function validateUser(user){
+    const schema=Joi.object({
+        name:Joi.string().min(10).max(50).required(),
+        email:Joi.string().email().required(),
+        password:Joi.string().min(8).max(255).required()
+    });
+    return await schema.validateAsync(user);
+}
+
+module.exports.User=User;
+module.exports.validateUser=validateUser;
