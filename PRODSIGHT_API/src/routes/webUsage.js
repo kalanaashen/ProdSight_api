3efcca9db7514de3c428d93cf1506f00378795d3
kalanaashen <northernwolf14742@gmail.com>
2026-05-21 16:46:21 +0530

    fix mongodb url to atlas

---
+++ PRODSIGHT_API/src/routes/webUsage.js
@@ -0,0 +1,16 @@
+const {getWebUsageByUserId, createWebUsage, getWebUsage} = require("../controllers/webUsageController");
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+
+router.get("/", auth, getWebUsage);
+
+router.get("/:userId", auth, getWebUsageByUserId);
+
+router.post("/", auth, createWebUsage);
+
+module.exports = router;
+
+
+
+
